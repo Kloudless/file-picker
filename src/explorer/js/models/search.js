df473be3b88dd9a5b182d3ad9067f85975a41ff5
Vinod Chandru <vinod.chandru@gmail.com>
2015-05-01 16:32:35 -0700

    Tabs to spaces.

--- src/explorer/js/models/search.js
+++ src/explorer/js/models/search.js
@@ -1,40 +1,39 @@
-(function(){
-	'use strict';
+(function() {
+  'use strict';
 
-	define(['vendor/knockout', 'vendor/loglevel', 'jquery', 'config'],
-			function(ko, logger, $, config){
+  define(['vendor/knockout', 'vendor/loglevel', 'jquery', 'config'],
+    function(ko, logger, $, config) {
 
-		//Create a search object
-		var Search = function(account, account_key, query){
-			this.account = account;
-			this.key = account_key
-			this.q = query;
-			this.results = null;
-			this.request = null;
-		};
+    //Create a search object
+    var Search = function(account, account_key, query) {
+      this.account = account;
+      this.key = account_key
+      this.q = query;
+      this.results = null;
+      this.request = null;
+    };
 
-		Search.prototype.getSearch = function(callback){
-			var self = this;
+    Search.prototype.getSearch = function(callback) {
+      var self = this;
 
-			self.request = $.ajax({
-				url: config.base_url + '/v0/accounts/' + self.account + "/search/?q=" + self.q,
-				type: 'GET',
-				headers: {
-					Authorization: 'AccountKey ' + self.key
-				},
-				success: function(data){
-					self.results = data;
-					logger.debug('Search results on', self.q, ':', self.results);
-					callback();
-				},
-				error: function(){
-					alert("Error in search request!");
-				},
-				datatype: 'json'
-			})
-		};
+      self.request = $.ajax({
+        url: config.base_url + '/v0/accounts/' + self.account + "/search/?q=" + self.q,
+        type: 'GET',
+        headers: {
+          Authorization: 'AccountKey ' + self.key
+        },
+        success: function(data) {
+          self.results = data;
+          logger.debug('Search results on', self.q, ':', self.results);
+          callback();
+        },
+        error: function() {
+          alert("Error in search request!");
+        },
+        datatype: 'json'
+      })
+    };
 
-		return Search;
-
-	})
+    return Search;
+  })
 })();
